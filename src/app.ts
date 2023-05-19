2243ad0837db6faf53a71c439fe96edfbc97a1a7
jaakumasi <jeromeakumasi01@gmail.com>
2023-05-19 10:24:05 +0000

    route imports in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
 import { NODE_ENV, PORT } from "./constants";
 import express from "express";
+import { authenticate, dbConnect } from "./middleware";
+import { assembly, login } from "./routes";
 const cors = require("cors");
+const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const app = express();
 app.use(cors({ origin: "*" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(dbConnect);
+app.use(authenticate);
+
+app.post("/login", login);
+app.all("/assembly", assembly);
 
 
 app.listen(process.env[PORT] || 38388 || 50060,
