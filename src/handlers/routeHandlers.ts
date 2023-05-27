d68d2d5524d9edd707e4e0ee078b2e0c56d04ed4
jaakumasi <jeromeakumasi01@gmail.com>
2023-05-27 19:19:09 +0000

    modded assembly post handler

--- src/handlers/routeHandlers.ts
+++ src/handlers/routeHandlers.ts
@@ -2,14 +2,19 @@ import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import crypto from "crypto";
 import { MEMBER_ID } from "../constants";
-import { getAssemblyMembers, getPayload } from "./utilities";
 import { Admin } from "../models/admins";
 import { Assembly } from "../models/assembly";
+import {
+    generateAssemblyCode,
+    getAssemblyMembers,
+    getPayload
+} from "./utilities";
 
 /* Admin Handlers */
 export const handlePostAdmin = async (req: Request, res: Response) => {
     const { member_id, is_superadmin } = req.body;
-    const randomPassword = crypto.randomBytes(2).toString("hex");
+    const randomBytes = crypto.randomBytes(3);
+    const randomPassword = randomBytes.readUintBE(0, 2).toString();
     const hashedPassword = await bcrypt.hash(randomPassword, 10);
     const newAdmin = new Admin({
         password: hashedPassword,
@@ -17,26 +22,36 @@ export const handlePostAdmin = async (req: Request, res: Response) => {
         is_superadmin,
     });
     await newAdmin.save();
-    return res.status(200).json({ randomPassword });
+
+    // TODO: send password to as SMS to phone
+
+    return res.sendStatus(200);
 }
 
 export const handleGetAdmin = async (req: Request, res: Response) => {
     const admins = await Admin.find();
-    return res.json({ data: admins });
+    return res.status(200).json({ data: admins });
 }
-/* END */
 
+/* END */
 
 /* Assembly Handlers */
 export const handlePostAssembly = async (req: Request, res: Response) => {
-    const { name, leaders, members } = req.body;
-    const assemblyLength = (await Assembly.find()).length;
+    const { name, annex_of, leaders, members } = req.body;
+    const assemblyNameAvailable = !(await Assembly.findOne({ name }));
+    if (!assemblyNameAvailable)
+        return res.status(401).json({ error: "Assembly name already exists!" });
+
+    const assembly_no = await generateAssemblyCode(name, annex_of);
     const newAssembly = new Assembly({
         name,
         leaders,
-        members,
-        assembly_no: assemblyLength.toString().padStart(2, "0")
-    })
+        assembly_no,
+        members
+    });
+    await newAssembly.save();
+
+    return res.status(200).json({ success: true });
 }
 
 export const handleGetAssembly = async (req: Request, res: Response) => {
@@ -48,7 +63,7 @@ export const handleGetAssembly = async (req: Request, res: Response) => {
     }
     // fetch all assemblies for superadmins
     if (admin.is_superadmin) {
-        const allAssemblies = await Assembly.find();        
+        const allAssemblies = await Assembly.find();
         const data = [];
         for (let i = 0; i < allAssemblies.length; i++) {
             data.push(getAssemblyMembers(allAssemblies[i]));
@@ -61,4 +76,5 @@ export const handleGetAssembly = async (req: Request, res: Response) => {
         return res.status(200).json({ data: getAssemblyMembers(assembly) });
     }
 }
+
 /* END */
